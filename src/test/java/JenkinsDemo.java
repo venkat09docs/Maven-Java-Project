5b091c07bf65234e8e125e766b90768b393c0a4e
venkat09 <venkat09docs@gmail.com>
2019-06-23 07:33:00 +0530

    Update JenkinsDemo.java

--- src/test/java/JenkinsDemo.java
+++ src/test/java/JenkinsDemo.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Created by burakergoren
