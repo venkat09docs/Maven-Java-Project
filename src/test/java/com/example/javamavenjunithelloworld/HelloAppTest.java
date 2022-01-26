031820dca1b616962141cd4cd191fcfc50362830
venkat09 <venkat09docs@gmail.com>
2022-01-26 21:35:15 +0530

    Update HelloAppTest.java

--- src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
@@ -45,7 +45,7 @@ public class HelloAppTest {
         PowerMockito.mockStatic(System.class);
 
         // Mock Hello used by HelloApp to throw the expected exception when invoked with setTimes(5).
-        Hello hi = mock(Hello.class);
+        Hello hi = mock(Hello.clas);
         doThrow(new IllegalArgumentException("Nope.")).when(hi).setTimes(5);
         // Sneakily insert our fake Hello class when it is created.
         whenNew(Hello.class).withNoArguments().thenReturn(hi);
